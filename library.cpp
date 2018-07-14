72a6e373048d8a0047493312e51bcca8477bdac9
kepatoto <kepatoto@gmail.com>
2018-07-13 22:52:35 -0400

    have enemy data reading in, just one for now

--- library.cpp
+++ library.cpp
@@ -2,6 +2,7 @@
 #include <Wincon.h>
 #include <iostream>
 #include <cmath>
+#include <wingdi.h>
 
 //I don't know if this is the right way to do a struct, seems wrong since all of
 // my members (is that what we call them?) are pointers.
@@ -59,11 +60,13 @@ DWORD WINAPI hackthread(LPVOID param)
     uintptr_t nameOffs[] = { 0x225 };
     uintptr_t teamOffs[] = { 0x032c }; // 0 red 1 blue
     uintptr_t healthOffs[] = { 0xf8 };
+    uintptr_t currEnOffs[] = { 0x4 };
 
     EntityData	my;
 
     uintptr_t pLocZAdd, pLocXAdd, pLocYAdd, aimXAdd, aimYAdd, myTeamAdd = 0;  // where we are standing
-    uintptr_t entHealthAdd, entNameAdd, entTeamAdd, entZAdd, entXAdd, entYAdd, currentEntAdd = 0;  // this will use the same offsets as plyZ, but the baseAddress of who we are targeting
+    uintptr_t entHealthAdd, entNameAdd, entTeamAdd, entZAdd, entXAdd, entYAdd = 0;
+    uintptr_t* currentEntAdd = 0;  // this will use the same offsets as plyZ, but the baseAddress of who we are targeting
 
     my.health = (int*)addressFinder(localPlayerAddress,healthOffs);
     my.fPosX = (float*)addressFinder(localPlayerAddress,xPosOffs);
@@ -82,10 +85,12 @@ DWORD WINAPI hackthread(LPVOID param)
 //    debug to 0; press f6
     BOOL debug = 1;
 
-    while (true)
+    // So we can break from the infinite loop
+    while (!GetAsyncKeyState(VK_F3) || !debug)
     {
         printf_s("kiyip's first internal aimbot\n");
-        printf_s("press f3 to stop the aimbot\n");
+//        printf_s("press f3 to stop the aimbot\n");
+        printf_s("to turn off the cheat press f3\nto aim use left alt or right click\n");
         printf_s("------------------------------\n");
 //       my.health = 9999;
 //        *health = 9999;
@@ -100,15 +105,37 @@ DWORD WINAPI hackthread(LPVOID param)
 
 
 
+        int i = 0;
+        float dist = 0.0f;
+        float minDist = 99999999.0;
+        bool aimToggle, inAimFov, found = false;
+        uintptr_t entNum = 0x1;//typically this should start at one
+
+        EntityData enemy;
+        currEnOffs[entNum] = entSize * entNum;
+        currentEntAdd = (uintptr_t *)addressFinder(entArrayBaseAdd, currEnOffs);
+        printf_s("%x\n", entArrayBaseAdd);
+        printf_s("%x\n", currentEntAdd);
+        enemy.health = (int*)addressFinder(currentEntAdd,healthOffs);
+        printf_s("health: %i\n", *enemy.health);
+
+
+        if (GetAsyncKeyState(VK_RBUTTON) || GetAsyncKeyState(VK_LCONTROL)) {
+            while (i < 31 && !GetAsyncKeyState(VK_F3) ) {//I know you shouldn't need the async, but better than an infinite loop
+                //TODO: find the closest entity
+
+                EntityData enemy;
+                i++;
+            }
+            //TODO: aim at the nearest entity
+            printf_s("AIMING\n");
+
+        }
 
 
 
 
 
-
-
-        // So we can break from the infinite loop
-        if (GetAsyncKeyState(VK_F3) || !debug) break;
 
         Sleep(10);
         system ("CLS");
